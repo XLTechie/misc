6e53dcec333b1476aafbd50b2e40e46d097d01f3
Luke Davis <XLTechie+github@newanswertech.com>
2023-06-23 06:16:07 -0400

    Change again. Try block for logging practically wraps everything.

--- main.py
+++ main.py
@@ -1,47 +1,49 @@
 import os
-import logging as log
+import logging
 from time import strftime
 LOG_FILE = r"%temp%\testApp.log"
 
-# Was artifact watcher 2
-import wx
-import github
-import threading
+try:
+	logging.basicConfig(level=logging.INFO, filename=os.path.abspath(os.path.expandvars(LOG_FILE)), filemode="w")
+	logging.info("Log started at " + strftime("%x %X"))
+
+	# Was artifact watcher 2
+	import wx
+	import github
+	import threading
 
 
-class App(wx.App):
-	def OnInit(self):
-		self.frame = wx.Frame(None, title="Artifact Downloader")
-		self.frame.Show()
-		self.repository = github.Repository("XLTechie", "misc")
-		self.artifacts_folder = os.path.expandvars(r"%temp%\artifacts")
-		self.github_thread = threading.Thread(target=self._watch_repository)
-		self.github_thread.daemon = True
-		self.github_thread.start()
-		return True
+	class App(wx.App):
+		def OnInit(self):
+			self.frame = wx.Frame(None, title="Artifact Downloader")
+			self.frame.Show()
+			self.repository = github.Repository("XLTechie", "misc")
+			self.artifacts_folder = os.path.expandvars(r"%temp%\artifacts")
+			self.github_thread = threading.Thread(target=self._watch_repository)
+			self.github_thread.daemon = True
+			self.github_thread.start()
+			return True
 
-	def _watch_repository(self):
-		while True:
-			for workflow_run in self.repository.get_workflow_runs():
-				for artifact in workflow_run.artifacts:
-					filename = artifact.name
-					filepath = os.path.join(self.artifacts_folder, filename)
-					if not os.path.exists(filepath):
-						artifact.download(filepath)
+		def _watch_repository(self):
+			while True:
+				for workflow_run in self.repository.get_workflow_runs():
+					for artifact in workflow_run.artifacts:
+						filename = artifact.name
+						filepath = os.path.join(self.artifacts_folder, filename)
+						if not os.path.exists(filepath):
+							artifact.download(filepath)
 
-def main():
-	app = App()
-	app.MainLoop()
+	def main():
+		app = App()
+		app.MainLoop()
 
-if __name__ == "__main__":
-	try:
-		log.basicConfig(level=log.INFO, filename=os.path.abspath(os.path.expandvars(LOG_FILE)), filemode="w")
-		log.info("Log started at " + strftime("%x %X"))
+	if __name__ == "__main__":
 		main()
-	except Exception as e:
-		log.exception("Exception occurred")
-		log.info("Terminating in failure.")
-	else:
-		log.info("Terminating successfully.")
-	finally:
-		log.info("Shutting down.")
+
+except Exception as e:
+	logging.exception("Exception occurred")
+	logging.info("Terminating in failure.")
+else:
+	logging.info("Terminating successfully.")
+finally:
+	logging.info("Shutting down.")
